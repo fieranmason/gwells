887fca6bbd12cb85196e85cadec1ea4b72118b1a
Fieran Mason <fieranmason@gmail.com>
2017-12-05 09:37:49 -0800

    Cleaned up replication script to address memory outages

--- database/cron/db-replicate.sh
+++ database/cron/db-replicate.sh
@@ -12,19 +12,25 @@
 export PGPASSWORD=$DATABASE_PASSWORD
 cd /opt/app-root/src/database/code-tables/
 
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f clear-tables.sql
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -c 'vacuum;'
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f  data-load-static-codes.sql
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f clear-tables.sql &&
 
-cd /opt/app-root/src/database/scripts/
+for table in $(psql -U postgres --tuples-only -c "SELECT schemaname || '.'
+|| tablename FROM pg_tables WHERE tablename NOT IN ('pg_authid', 'pg_db_role_setting', 'pg_tablespace', 'pg_pltemplate', 'pg_auth_members', 'pg_shdepend', 'pg_shdescription', 'pg_replication_origin', 'pg_shseclabel', 'pg_database')") ;
+do psql -U postgres -c "VACUUM ANALYZE $table"; done
 
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f create-xform-gwells-well-ETL-table.sql
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f populate-xform-gwells-well.sql
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f populate-gwells-well-from-xform.sql
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_screens.sql
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_production_data.sql
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_casings.sql
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_perforations.sql
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_aquifer_wells.sql
-psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_lithology_descriptions.sql
+sleep 30 &&
+
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f  data-load-static-codes.sql &&
+
+cd /opt/app-root/src/database/scripts/ &&
+
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f create-xform-gwells-well-ETL-table.sql &&
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f populate-xform-gwells-well.sql &&
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f populate-gwells-well-from-xform.sql &&
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_screens.sql &&
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_production_data.sql &&
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_casings.sql &&
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_perforations.sql &&
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_aquifer_wells.sql &&
+psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER -f migrate_lithology_descriptions.sql &&
 exit 0
